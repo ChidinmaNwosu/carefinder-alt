bf6685e568a6d2145a0b44cb22727397f86f86cb
Nwosu Chidinma <chidinmaifynwosu@gmail.com>
2024-08-03 05:18:41 +0100

    I implemented the markdown suppport

--- src/app/(adminPortal)/admin-dashboard/page.tsx
+++ src/app/(adminPortal)/admin-dashboard/page.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+'use client'
+import React, {useEffect} from 'react'
 import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 
 function AdminDashboard():React.JSX.Element {
-  // Initialize the Editor.js instance with the provided element ID
-  const editor = new Editor({
-    el: document.querySelector('#editor'),
-    height: '600px',
-    initialEditType: 'markdown',
-    previewStyle: 'vertical'
-  });
+  //Initialize the Editor.js instance with the provided element ID
+  useEffect (()=>{
+    
+    const editor = new Editor({
+      el: document.querySelector('#editor'),
+      height: '500px',
+      width: '500px',
+      initialEditType: 'markdown',
+      previewStyle: 'vertical',
+      initialValue: 'content',
+      placeholder: 'Please Enter Text...'
+    });
+
+  
+  
+    console.log(editor.getMarkdown());
+  },[]);
+    
+
 
   return (
     <div className="h-screen w-screen">
@@ -18,12 +33,12 @@ function AdminDashboard():React.JSX.Element {
                 <p className="font-medium text-xl">Welcome to the admin dashboard!</p>
             </div>
             {/* Add your admin dashboard components here */}
-              <div className="className" id="editor">
-                
-              </div>
+           
+              <div className="className" id="editor"></div>
         </div>
     </div>
   )
 }
+
 
 export default AdminDashboard;
